a260acd498ad3dba99d9de794a4c87ef6b085910
MoGa <MoGa@DESKTOP-HSMA4DI>
2025-01-02 17:55:36 +0200

    Q01

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        #region Q01
+        enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+        } 
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            
+
+            for(int i =0; i<7; i++)
+            {
+                WeekDays days = (WeekDays)i;
+                Console.WriteLine(days);
+            }
+            #endregion
         }
     }
 }
