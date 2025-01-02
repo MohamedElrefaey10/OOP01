153ce9eb772708809cbe34b1e51136042a5b8a57
MoGa <MoGa@DESKTOP-HSMA4DI>
2025-01-02 22:05:22 +0200

    Q03

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -15,12 +15,26 @@
         //} 
         #endregion
         #region Q02
-        enum Season
+        //enum Season
+        //{
+        //    Spring=10, Summer, Autumn, Winter
+        //}
+
+
+        #endregion
+        #region Q03
+        [Flags]
+        enum permissions:byte
         {
-            Spring=10, Summer, Autumn, Winter
+            Read=1, write=2, Delete=4, Execute=8
         }
-
-
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public decimal Salary;
+            public permissions permissions;
+        }
         #endregion
         static void Main(string[] args)
         {
@@ -42,34 +56,60 @@
             #region Q02
 
 
-            Console.WriteLine("Enter A Month ");
+            //Console.WriteLine("Enter A Month ");
 
 
-            Enum.TryParse<Season>(Console.ReadLine(), true, out Season result);
+            //Enum.TryParse<Season>(Console.ReadLine(), true, out Season result);
 
 
-            switch (result)
+            //switch (result)
+            //{
+            //    case Season.Spring:
+            //        Console.WriteLine("spring march to may");
+            //        break;
+            //    case Season.Summer:
+            //        Console.WriteLine("summer june to august");
+            //        break;
+            //    case Season.Winter:
+            //        Console.WriteLine("winter December to February");
+            //        break;
+            //    case Season.Autumn:
+            //        Console.WriteLine("autumn September to November");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid");
+            //        break;
+            //}
+
+
+
+
+            #endregion
+
+            #region Q03
+
+            Employee employee = new Employee();
+            employee.Name = "Ali";
+            employee.permissions = (permissions)3;
+            Console.WriteLine(employee.permissions); // Read, write
+
+            employee.permissions = employee.permissions ^ permissions.Delete;
+            Console.WriteLine(employee.permissions); // Read, write ,Delete
+
+            employee.permissions = employee.permissions ^ permissions.Delete;
+            Console.WriteLine(employee.permissions); // Read, write 
+
+            employee.permissions = employee.permissions | permissions.Execute;
+            Console.WriteLine(employee.permissions); // Read, write, Execute
+
+            if((employee.permissions & permissions.Execute) == permissions.Execute)
             {
-                case Season.Spring:
-                    Console.WriteLine("spring march to may");
-                    break;
-                case Season.Summer:
-                    Console.WriteLine("summer june to august");
-                    break;
-                case Season.Winter:
-                    Console.WriteLine("winter December to February");
-                    break;
-                case Season.Autumn:
-                    Console.WriteLine("autumn September to November");
-                    break;
-                default:
-                    Console.WriteLine("Invalid");
-                    break;
+                Console.WriteLine("Already exists");
             }
-
-
-
-
+            else
+            {
+                employee.permissions = employee.permissions ^ permissions.Execute;
+            }
             #endregion
         }
     }
