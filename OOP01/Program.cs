65770f618203441b4244ee62cb46f7e193ce2bda
MoGa <MoGa@DESKTOP-HSMA4DI>
2025-01-02 21:26:32 +0200

    Q02

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -3,27 +3,73 @@
     internal class Program
     {
         #region Q01
-        enum WeekDays
+        //enum WeekDays
+        //{
+        //    Monday,
+        //    Tuesday,
+        //    Wednesday,
+        //    Thursday,
+        //    Friday,
+        //    Saturday,
+        //    Sunday,
+        //} 
+        #endregion
+        #region Q02
+        enum Season
         {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday,
-        } 
+            Spring=10, Summer, Autumn, Winter
+        }
+
+
         #endregion
         static void Main(string[] args)
         {
             #region Q01
-            
 
-            for(int i =0; i<7; i++)
+
+            //for(int i =0; i<7; i++)
+            //{
+            //    WeekDays days = (WeekDays)i;
+            //    Console.WriteLine(days);
+            //}
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+
+            #endregion
+
+            #region Q02
+
+
+            Console.WriteLine("Enter A Month ");
+
+
+            Enum.TryParse<Season>(Console.ReadLine(), true, out Season result);
+
+
+            switch (result)
             {
-                WeekDays days = (WeekDays)i;
-                Console.WriteLine(days);
+                case Season.Spring:
+                    Console.WriteLine("spring march to may");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("summer june to august");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("winter December to February");
+                    break;
+                case Season.Autumn:
+                    Console.WriteLine("autumn September to November");
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
             }
+
+
+
+
             #endregion
         }
     }
